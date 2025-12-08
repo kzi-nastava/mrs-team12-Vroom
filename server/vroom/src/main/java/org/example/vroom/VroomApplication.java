e8667f4fc64884041489aae350736bc696f971dd
teofeel <teodor.perun@gmail.com>
2025-12-08 16:46:41 +0100

    Init server

---
+++ server/vroom/src/main/java/org/example/vroom/VroomApplication.java
@@ -0,0 +1,13 @@
+package org.example.vroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VroomApplication.class, args);
+    }
+
+}
