b29b88dafb1b50c30585bb5a60ba4654c2563ae2
teofeel <teodor.perun@gmail.com>
2025-12-13 17:34:05 +0100

    Login form
    - Create login form on web
    - Designed by figma file
    - Prepared on login and on forgot password methods for later

---
+++ web/vroom/src/app/features/login/login.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+
+export class Login {
+  email: String = ''
+  password: String = ''
+  error: String = ''
+
+  onForgotPassword(): void{
+    console.log(this.email)
+  }
+
+  onLogin(): void{
+    console.log(this.email, this.password)
+  }
+}
