2ce85d203676d2fd2ce613e0f06f70b6760d87e4
Лазар Вилотић <163160440+LazarVilotic@users.noreply.github.com>
2025-12-14 20:28:34 +0100

    Every Registered User Profile View
    - Created profile view on web
    - Designed by figma file
    - Done for both driver and passenger

--- web/vroom/src/app/app.routes.ts
+++ web/vroom/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {Login} from './features/login/login'
+import { Profile } from './features/profile/profile';
 
 export const routes: Routes = [
-    {path: 'login', component: Login}
+    {path: 'login', component: Login},
+    { path: 'profile', component: Profile }
 ];
