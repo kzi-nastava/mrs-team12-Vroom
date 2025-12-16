3dbd491cc37f48858579ac12661b4d2120a1ad0e
isidorakorda <isidorakorda24@gmail.com>
2025-12-17 00:34:49 +0100

    Made a navbar with buttons for KT1 components
    Finished making the navbar, made it appear on every page, connected the other pages to the buttons

--- web/vroom/src/app/app.routes.ts
+++ web/vroom/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import {Register} from './features/register/register'
 import {Login} from './features/login/login'
 import { Profile } from './features/profile/profile';
+import {Navbar} from './features/navbar/navbar'
 
 export const routes: Routes = [
     {path: 'login', component: Login},
-    { path: 'profile', component: Profile },
+    {path: 'profile', component: Profile },
     {path: 'register', component: Register}
 ];
