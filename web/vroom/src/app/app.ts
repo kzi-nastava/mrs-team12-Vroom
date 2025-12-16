560822165c670c7519b69f5f02490816e1b21d39
isidorakorda <isidorakorda24@gmail.com>
2025-12-17 00:34:49 +0100

    Made a navbar with buttons for KT1 components
    Finished making the navbar, made it appear on every page, connected the other pages to the buttons

--- web/vroom/src/app/app.ts
+++ web/vroom/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './features/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  showNavbar = true;
   protected readonly title = signal('vroom');
 }
