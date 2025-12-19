37e73707939e86ea26d4800a76f3a2fbad5a3c2a
teofeel <teodor.perun@gmail.com>
2025-12-19 08:53:10 +0100

    Finished forgot password page on WEB
    - Create forgot password forms
    - Connected login and forgot password
    - Fixed fonts
    - Fixed Register page submit button

--- web/vroom/src/app/features/login/login.ts
+++ web/vroom/src/app/features/login/login.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import {FormsModule} from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +15,12 @@ export class Login {
   password: String = ''
   error: String = ''
 
+
+  constructor(private router: Router){}
+
   onForgotPassword(): void{
     console.log(this.email)
+    this.router.navigate(['/forgot-password'])
   }
 
   async onLogin(): Promise<void>{
