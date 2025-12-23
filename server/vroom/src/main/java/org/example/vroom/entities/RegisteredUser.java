8b3429cf4aca442ad0ca1a63912f36ed18e1703f
teofeel <teodor.perun@gmail.com>
2025-12-23 16:57:54 +0100

    Fixed issue with lombok

--- server/vroom/src/main/java/org/example/vroom/entities/RegisteredUser.java
+++ server/vroom/src/main/java/org/example/vroom/entities/RegisteredUser.java
@@ -2,30 +2,17 @@ package org.example.vroom.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @DiscriminatorValue("REGISTERED_USER")
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
 public class RegisteredUser extends User{
     //@Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private String status;      // change with user status enum
 
-
-    @Builder
-    public RegisteredUser(
-            String email,
-            String password,
-            String firstName,
-            String lastName,
-            String address,
-            String phoneNumber,
-            String status
-    )
-    {
-        super(email, password, firstName, lastName, address, phoneNumber, null, null);
-        this.status = status;
-    }
 }
