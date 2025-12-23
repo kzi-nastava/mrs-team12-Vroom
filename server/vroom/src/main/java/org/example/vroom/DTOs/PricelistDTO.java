8d40ae776f8be9103dddd5cbbf01fa74d2bd96fb
Лазар Вилотић <163160440+LazarVilotic@users.noreply.github.com>
2025-12-23 18:27:39 +0100

    [Server] Admin, Pricelist and Enums
    - Created entities
    - Created DTOs
    - Created Mapper for both
    -Created all the required enums

---
+++ server/vroom/src/main/java/org/example/vroom/DTOs/PricelistDTO.java
@@ -0,0 +1,18 @@
+package org.example.vroom.DTOs;
+
+
+import lombok.*;
+import org.example.vroom.enums.VehicleType;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PricelistDTO {
+
+    private Map<VehicleType, Float> typePrice;
+
+    private float pricePerKm;
+}
