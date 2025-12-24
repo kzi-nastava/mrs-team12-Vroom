7d324fba4d44db3a76a8e83f2906c4e779362272
isidorakorda <isidorakorda24@gmail.com>
2025-12-24 02:22:32 +0100

    Finished writing models and DTOs for driver and vehicle

---
+++ server/vroom/src/main/java/org/example/vroom/DTOs/DriverDTO.java
@@ -0,0 +1,17 @@
+package org.example.vroom.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.example.vroom.enums.DriverStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public class DriverDTO  extends UserDTO {
+    private DriverStatus status;
+    private Long ratingCount;
+    private Long ratingSum;
+    private VehicleDTO vehicle;
+}
