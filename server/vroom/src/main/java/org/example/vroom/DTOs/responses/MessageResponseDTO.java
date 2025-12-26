f6390eb80f8e292ae9fe10ba84b735f217513891
teofeel <teodor.perun@gmail.com>
2025-12-26 11:53:26 +0100

    Finished endpoints, req&res DTOs, fixed small issues

---
+++ server/vroom/src/main/java/org/example/vroom/DTOs/responses/MessageResponseDTO.java
@@ -0,0 +1,14 @@
+package org.example.vroom.DTOs.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponseDTO {
+    private String message;
+}
