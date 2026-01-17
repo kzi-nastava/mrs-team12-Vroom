751789ec74666dc5927bc51b157fee7387e31c76
teofeel <teodor.perun@gmail.com>
2026-01-17 13:02:53 +0100

    Created chage driver status for login

--- web/vroom/src/app/features/navbar/navbar.ts
+++ web/vroom/src/app/features/navbar/navbar.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { CommonModule } from '@angular/common';
+import { ChangeDriverStatus } from '../change-driver-status/change-driver-status';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, ChangeDriverStatus],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
