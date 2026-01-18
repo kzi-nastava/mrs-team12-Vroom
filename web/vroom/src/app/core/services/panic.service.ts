c8a72359b3d51a47152154049b2a44abcb266c1a
teofeel <teodor.perun@gmail.com>
2026-01-18 17:57:51 +0100

    Connected panic with server, improved route estimation

--- web/vroom/src/app/core/services/panic.service.ts
+++ web/vroom/src/app/core/services/panic.service.ts
@@ -1,10 +1,11 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { CancelRideRequestDTO } from "../models/ride/requests/cancel-ride-req.dto";
 import { MessageResponseDTO } from "../models/message-response.dto";
 import { Observable } from "rxjs";
 import { StopRideRequestDTO } from "../models/ride/requests/stop-ride-req.dto";
 import { PanicRequestDTO } from "../models/panic/requests/panic-request.dto";
+import { PanicNotificationDTO } from "../models/panic/responses/panic-notification.dto";
 
 @Injectable({
     providedIn: "root"
@@ -18,11 +19,12 @@ export class PanicService{
         return this.http.post<MessageResponseDTO>(`${this.panicUrl}/ride/${rideId}`, data)
     }
 
-    getActivePanicRequests(){
-        
+    getActivePanicRequests(): Observable<PanicNotificationDTO[]>{
+        const params = new HttpParams().set('active', true);
+        return this.http.get<PanicNotificationDTO[]>(`${this.panicUrl}`, {params})
     }
 
-    resolvePanicRequest(rideID: string){
-
+    resolvePanicRequest(panicID: number | string): Observable<MessageResponseDTO>{
+        return this.http.put<MessageResponseDTO>(`${this.panicUrl}/${panicID}/resolve`, {})
     }
 }
