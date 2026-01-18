aa8f703eab233d087e298256ba53d6e6b1765f92
Лазар Вилотић <163160440+LazarVilotic@users.noreply.github.com>
2026-01-18 01:27:43 +0100

    [Web][Server](fix) RegUser Profile functional
    -JwtAuthFilter profiles commented
    -ProfileController fixed
    -added debug to server properties
    -everything on web updated

--- server/vroom/src/main/java/org/example/vroom/controllers/UserProfileController.java
+++ server/vroom/src/main/java/org/example/vroom/controllers/UserProfileController.java
@@ -1,22 +1,27 @@
 package org.example.vroom.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.example.vroom.DTOs.RegisteredUserDTO;
 import org.example.vroom.services.RegisteredUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(
+        origins = "http://localhost:4200",
+        allowCredentials = "true"
+)
 @RestController
 @RequestMapping("/api/profile/user")
 public class UserProfileController {
 
-    RegisteredUserService registeredUserService;
+    //RegisteredUserService registeredUserService;
+    private final RegisteredUserService registeredUserService;
 
+    public UserProfileController(RegisteredUserService registeredUserService) {
+        this.registeredUserService = registeredUserService;
+    }
     @GetMapping("/me")
     public ResponseEntity<RegisteredUserDTO> getMyProfile(
             @AuthenticationPrincipal UserDetails userDetails
