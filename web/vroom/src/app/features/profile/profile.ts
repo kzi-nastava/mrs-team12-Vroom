2feb5c86606f103ffae9ee46fae6213ec37f1cb4
Лазар Вилотић <163160440+LazarVilotic@users.noreply.github.com>
2026-01-18 01:27:43 +0100

    [Web][Server](fix) RegUser Profile functional
    -JwtAuthFilter profiles commented
    -ProfileController fixed
    -added debug to server properties
    -everything on web updated

--- web/vroom/src/app/features/profile/profile.ts
+++ web/vroom/src/app/features/profile/profile.ts
@@ -1,69 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ProfileService } from '../profile/profile.service'; // ← PUTANJA!
+import { ProfileService } from '../profile/profile.service'; 
+import { Observable, tap } from 'rxjs';
+import {Profile as ProfileModel} from '../profile/profile.model'; 
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrls: ['./profile.css'],
 })
 export class Profile implements OnInit {
 
   userRole: 'DRIVER' | 'PASSENGER' = 'PASSENGER';
+  profile$!: Observable<ProfileModel>;   
+  profileData!: ProfileModel;             
+  editMode = false;
 
-  firstName = '';
-  lastName = '';
-  address = '';
-  phone = '';
-  email = '';
+  constructor(private profileService: ProfileService) { }
 
-  vehicle: any;
-  activeHoursLast24h = 0;
+  ngOnInit(): void {
+    this.loadProfile();
+  }
 
-  constructor(private profileService: ProfileService) {}
+  loadProfile() {
+    this.profile$ = this.profileService.getMyProfile()
+      .pipe(
+        tap(data => this.profileData = { ...data }) 
+      );
+  }
 
-ngOnInit(): void {
-  this.profileService.getMyProfile().subscribe(profile => {
-    this.firstName = profile.firstName;
-    this.lastName = profile.lastName;
-    this.address = profile.address;
-    this.phone = profile.phoneNumber;
-    this.email = profile.email;
-
-    if (profile.vehicle) {
-      this.userRole = 'DRIVER';
-      this.vehicle = profile.vehicle;
-      this.activeHoursLast24h = profile.activeHoursLast24h ?? 0;
+  onChange() {
+    if (this.editMode) {
+      this.profileService.updateMyProfile(this.profileData)
+        .subscribe({
+          next: updated => {
+            console.log('Profile saved', updated);
+            this.profileData = { ...updated }; 
+            this.loadProfile();
+          },
+          error: err => console.error('Error saving profile', err)
+        });
     }
-  });
+    this.editMode = !this.editMode;
+  }
 }
-
-fillProfile(profile: any) {
-  this.firstName = profile.firstName;
-  this.lastName = profile.lastName;
-  this.address = profile.address;
-  this.phone = profile.phone;
-  this.email = profile.email;
-}
-
-onChange(): void {
-  const payload = {
-    firstName: this.firstName,
-    lastName: this.lastName,
-    address: this.address,
-    phoneNumber: this.phone, 
-    email: this.email
-  };
-
-  this.profileService.updateMyProfile(payload)
-    .subscribe({
-      next: updated => console.log('Updated profile', updated),
-      error: err => console.error('Update failed', err)
-    });
-}
-}
-
-
