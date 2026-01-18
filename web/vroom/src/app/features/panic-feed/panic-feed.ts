f7701fe8d6450eec7dba6aebfed78da3c7c75b9d
teofeel <teodor.perun@gmail.com>
2026-01-18 17:57:51 +0100

    Connected panic with server, improved route estimation

--- web/vroom/src/app/features/panic-feed/panic-feed.ts
+++ web/vroom/src/app/features/panic-feed/panic-feed.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PanicService } from '../../core/services/panic.service';
 import { Router } from '@angular/router';
+import { PanicNotificationDTO } from '../../core/models/panic/responses/panic-notification.dto';
 
 @Component({
   selector: 'app-panic-feed',
@@ -10,29 +11,54 @@ import { Router } from '@angular/router';
   styleUrl: './panic-feed.css',
 })
 export class PanicFeed implements OnInit{
-  panicAlerts = [{rideID: 1, activatedBy: 'user', time: '12-03-15'}, {rideID: 2, activatedBy: 'user', time: '12-03-15'}]
+  isLoadingData: boolean = false;
+  isResolving: boolean = false;
+  panicAlerts: PanicNotificationDTO[] = [];
 
-  constructor(private panicService: PanicService, private router: Router){}
+  constructor(private panicService: PanicService, private router: Router, private cdr: ChangeDetectorRef){}
 
-  onResolved(rideID: string | number){
-    console.log('resolved', rideID)
-    this.panicAlerts = this.panicAlerts.filter(alert => alert.rideID != rideID)
-    this.panicService.resolvePanicRequest(rideID.toString())
+  onResolved(panicID: string | number){
+    this.isResolving = true
+    this.panicService.resolvePanicRequest(panicID).subscribe({
+        next: (response) => {
+            this.isResolving = false
+            this.panicAlerts = this.panicAlerts.filter(alert => alert.id !== panicID);
+            this.cdr.detectChanges()
+        },
+        error: (err) => {
+            this.isResolving = false
+            alert('Could not resolve panic alert.');
+            this.cdr.detectChanges()
+        }
+    });
   }
 
   ngOnInit(): void {
       // ucitati podatke u panicAlerts
       this.loadAlerts()
-      
   }
 
   private loadAlerts(){
-    console.log('loading alerts....')
-    // this.panicAlerts = this.panicService.getActivePanicRequests()
+    this.isLoadingData = true
+    this.panicService.getActivePanicRequests().subscribe({
+      next: (response: PanicNotificationDTO[]) => {
+        if(response === null){
+          alert('No new alerts')
+        }
+        this.panicAlerts = response
+        this.isLoadingData = false
+        this.cdr.detectChanges()
+      },
+      error: (e) => {
+        this.isLoadingData = false
+        alert('There has been an error with loading alerts')
+        this.cdr.detectChanges()
+      }
+    })
   }
 
   mapRedirect(rideID: string | number){
-    this.router.navigate(['/login']) // change to map and send data
+    this.router.navigate(['/']) // change to map and send data
   }
 
 }
