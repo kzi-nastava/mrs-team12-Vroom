7448f6cd4eeed9c0883b90654f39235f8490a4c3
isidorakorda <isidorakorda24@gmail.com>
2026-02-07 16:25:10 +0100

    Finished administrator active rides view
    Added page for all active rides with a search bar to filter drivers,
    Cards are clickable and lead to a map view with eta, route, current location...

--- server/vroom/src/main/java/org/example/vroom/DTOs/responses/ride/GetActiveRideInfoDTO.java
+++ server/vroom/src/main/java/org/example/vroom/DTOs/responses/ride/GetActiveRideInfoDTO.java
@@ -1,17 +1,21 @@
 package org.example.vroom.DTOs.responses.ride;
 
-import lombok.Builder;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class GetActiveRideInfoDTO {
+    private Long rideId;
     private String startAddress;
     private String endAddress;
     private LocalDateTime startTime;
     private String driverName;
     private String creatorName;
-    private List<String> passengerEmails;
 
 }
---
+++ web/vroom/src/app/core/models/admin/get-active-ride-info.dto.ts
@@ -0,0 +1,8 @@
+export interface GetActiveRideInfoDTO{
+    rideId : number;
+    startAddress : String;
+    endAddress : String;
+    startTime : Date;
+    driverName : String;
+    creatorName : String;
+}
