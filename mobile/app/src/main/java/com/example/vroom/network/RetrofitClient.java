fba387298cf2291a6c62017a9ec6f13b8935c7eb
isidorakorda <isidorakorda24@gmail.com>
2026-02-17 15:09:53 +0100

    Ride History issue wasnt loading rating

--- mobile/app/src/main/java/com/example/vroom/network/RetrofitClient.java
+++ mobile/app/src/main/java/com/example/vroom/network/RetrofitClient.java
@@ -30,8 +30,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Context mContext;
-    //private static final String BASE_URL = "http://192.168.0.110:8080/";
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://192.168.0.110:8080/";
+//    private static final String BASE_URL = "http://10.0.2.2:8080/";
     private static Retrofit retrofit = null;
 
     public static void init(Context context) {
